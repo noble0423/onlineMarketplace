adf9607784359acbf04cd09fdab2afd820861e65
noble0423 <ryan.noble82@gmail.com>
2018-09-24 16:34:52 -0500

    react-router is working for basic about page.

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import About from "./pages/About/About.js";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
+// import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div>
+          <Navbar />
+          <Wrapper>
+            {/* <About /> */}
+            <Route exact path="/" component={About} />
+            <Route exact path="/about" component={About} />
+          </Wrapper>
+          <Footer />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
