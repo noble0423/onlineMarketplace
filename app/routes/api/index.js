0747ca56815a6420fcc0d8b6951ff47ff7fe499b
noble0423 <ryan.noble82@gmail.com>
2018-09-27 12:46:40 -0500

    debugged api/index.js file...was showing 'router.use('/inventories', inventoryRoutes);'...changed inventories to read inventory. initial getWorks API call to load inventory is now working.

--- app/routes/api/index.js
+++ app/routes/api/index.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const inventoryRoutes = require("./inventories");
 
 // Inventory routes
-router.use("/inventories", inventoryRoutes);
+router.use("/inventory", inventoryRoutes);
 
 module.exports = router;
